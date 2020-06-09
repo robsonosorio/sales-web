d9b38eec2a85a6d95f7895f0217c02f0e65433d7
robsonosorio <robson.osorioo@gmail.com>
2020-06-08 22:09:47 -0300

    adaptação com MySql e primeira Migration

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Data;
 
 namespace SalesWebMvc
 {
@@ -33,12 +35,15 @@ namespace SalesWebMvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalesWebMvcContext>(options =>
+                    options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
+                       builder.MigrationsAssembly("SalesWebMvc"))); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
+        {            if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
